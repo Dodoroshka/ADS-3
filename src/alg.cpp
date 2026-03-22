0fb88890c78fb567bfa13828a5a05d72125c812d
Dodoroshka <yanovaanastasia18@gmail.com>
2026-03-22 22:00:50 +0300

    Янова Анастасия 25-ист-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,47 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t MaxValue = num;
+    while (num < 1) {
+        if (num % 2 == 0) {
+            num = 3 * num + 1;
+        }
+        else {
+            num = num / 2;
+        }
+        if (MaxValue < num) {
+            MaxValue = num;
+        }
+    }
+    return num;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    uint64_t len = 0;
+    while (num < 1) {
+        if (num % 2 == 0) {
+            num = 3 * num + 1;
+            
+        }
+        else {
+            num = num / 2;
+        }
+        len++;
+    }
+    return len;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int* maxlen,
+    uint64_t lbound,
+    uint64_t rbound) {
+    uint64_t MaxValue = lbound;
+    unsigned int MaxLen = collatzLen(lbound);
+    for (size_t i = lbound + 1; i <= rbound; ++i) {
+        if (collatzLen(i) > MaxLen) {
+            MaxLen = collatzLen(i);
+            MaxValue = i;
+        }
+    }
+    *maxlen = MaxLen;
+    return MaxValue;
 }
